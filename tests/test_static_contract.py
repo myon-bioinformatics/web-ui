d90ed61a5aa774755ab0d9237c13e825b8fed307
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-09-23 17:25:37 +0900

    fix: write parser state as valid Python lines

--- tests/test_static_contract.py
+++ tests/test_static_contract.py
@@ -27,7 +27,9 @@ class ContractParser(HTMLParser):
         self.hrefs = []
         self.classes = []
         self.ids = set()
-        self.labels_for = set()\n        self.stack = []\n        self.class_parents = {}
+        self.labels_for = set()
+        self.stack = []
+        self.class_parents = {}
 
     def handle_starttag(self, tag, attrs):
         values = dict(attrs)
